c536cb26a426044e09a290a10358a71b2bb7215f
kizailent <egami.t.ab@m.titech.ac.jp>
2025-02-21 22:32:14 +0900

    課題完成
    username=名無しを入れる

--- omikuji.js
+++ omikuji.js
@@ -1,9 +1,14 @@
 let username;
 let userresult;
+
 username = prompt("お名前を教えてください")
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random()*5);
+
+if (username == ""){
+    username = "名無し"
+}
 
 if (rand == 0) {
   userresult = "大吉";
